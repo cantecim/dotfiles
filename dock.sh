5fe2a9394ff4aeded8f7dad10030747a5421e58b
Can Tecim <can.tecim@gmail.com>
2019-03-06 12:11:54 +0300

    brew bundle and dock update

--- dock.sh
+++ dock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Siri.app"
+dockutil --no-restart --add "/Applications/Launchpad.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
@@ -9,6 +9,13 @@ dockutil --no-restart --add "/Applications/Notes.app"
 dockutil --no-restart --add "/Applications/App Store.app"
 dockutil --no-restart --add "/Applications/Discord.app"
 dockutil --no-restart --add "/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/WebStorm.app"
+dockutil --no-restart --add "/Applications/PyCharm.app"
+dockutil --no-restart --add "/Applications/PhpStorm.app"
+dockutil --no-restart --add "/Applications/DataGrip.app"
+dockutil --no-restart --add "/Applications/Sublime Text.app"
+dockutil --no-restart --add "/Applications/DevDocs.app"
+dockutil --no-restart --add "/Applications/Postman.app"
 dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 
