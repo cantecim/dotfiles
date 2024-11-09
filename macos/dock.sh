3dc4d2db15dec91f5d5741fe022cb1576d744eaa
Can Tecim <can.tecim@gmail.com>
2024-11-09 12:22:27 +0300

    feat(): set dock icons

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Launchpad.app"
+dockutil --no-restart --add "/Applications/WebStorm.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+# dockutil --no-restart --add "/Applications/Postman.app"
+dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Opera.app"
+dockutil --no-restart --add "/System/Applications/Notes.app"
+dockutil --no-restart --add "/Applications/RemNote.app"
+dockutil --no-restart --add "/Applications/Obsidian.app"
+dockutil --no-restart --add "/Applications/Microsoft To Do.app"
+dockutil --no-restart --add "/Applications/Discord.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
-dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
+# dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/System/Applications/System Settings.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add '~/Downloads' --view fan --display stack --allhomes
 
 killall Dock
